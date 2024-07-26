f2115b914f7329837d031f3f40348a948c9e1f25
Salvador Fernandez <salvadormatiasfernandez@gmail.com>
2024-07-26 06:31:25 -0300

    arme arrays

---
+++ main.js
@@ -0,0 +1,7 @@
+const divisas = [
+    {id:1, nombre:"Dolar", descripcion: "Moneda oficial Estadounidense", precio:1450, imagen:"https://img.freepik.com/foto-gratis/numero-banco_1232-3931.jpg"},
+    {id:1, nombre:"Euro", descripcion: "Moneda oficial Europea", precio:1633, imagen:"https://www.ceca.es/wp-content/uploads/2023/08/euros.jpg"},
+    {id:1, nombre:"Real", descripcion: "Moneda oficial Brasileña", precio:257, imagen:"https://media.istockphoto.com/id/1322173172/es/foto/en-esta-ilustración-fotográfica-se-muestran-ciento-doscientos-reales-y-una-moneda-de-un-real.jpg?s=612x612&w=0&k=20&c=9pG2yxdpFYe9j2oKboOqvwU9skbokYaIFW9Xn48FH_Y="},
+    {id:1, nombre:"Peso Uruguayo", descripcion: "Moneda oficial Uruguaya", precio:29, imagen:"https://st3.depositphotos.com/2870309/17334/i/450/depositphotos_173341030-stock-photo-uruguayan-pesos-a-background.jpg"},
+]
+console.log (divisas)
